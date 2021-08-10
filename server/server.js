3c4a5e7b73008a85cc165153df6fe30f162c3614
Megan Skippen <79444992+mskippen@users.noreply.github.com>
2021-08-11 03:52:05 +1000

    More fixes

--- server/server.js
+++ server/server.js
@@ -35,11 +35,15 @@ app.use(
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+    });
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 db();
 
