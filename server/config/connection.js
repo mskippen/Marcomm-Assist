8f36e633abf08c2833cc5ae6c98cea89bca1df8d
Megan Skippen <79444992+mskippen@users.noreply.github.com>
2021-08-11 04:37:03 +1000

    More fixes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // const connection = () =>
-  mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/marcomm_app", {
+  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/marcomm_app", {
       useNewUrlParser: true,
       useFindAndModify: true,
       useUnifiedTopology: true
